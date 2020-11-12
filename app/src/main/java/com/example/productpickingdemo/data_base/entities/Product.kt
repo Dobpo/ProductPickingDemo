fe29ce70671bc046481e7beba95236258549155b
Alexandr Kravchenko <alex.kravchenko@apptuns.com>
2020-11-12 03:11:26 +0200

    fix

--- app/src/main/java/com/example/productpickingdemo/data_base/entities/Product.kt
+++ app/src/main/java/com/example/productpickingdemo/data_base/entities/Product.kt
@@ -6,24 +6,26 @@ import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "products"/*, foreignKeys = [
+    tableName = "products", foreignKeys = [
         ForeignKey(
             entity = Order::class,
             parentColumns = arrayOf("id"),
-            childColumns = arrayOf("orderId"),
+            childColumns = arrayOf("order_id"),
             onDelete = ForeignKey.CASCADE
         ),
         ForeignKey(
             entity = Location::class,
             parentColumns = arrayOf("id"),
-            childColumns = arrayOf("locationId"),
+            childColumns = arrayOf("location_id"),
             onDelete = ForeignKey.CASCADE
         )
-    ]*/
+    ]
 )
 data class Product(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "order_id") val orderId: Int?,
+    @ColumnInfo(name = "location_id") val locationId: String?,
+    @ColumnInfo(name = "name") val name: Int?,
     @ColumnInfo(name = "barcode") val barcode: String?,
     @ColumnInfo(name = "request_quantity") val requestQuantity: Int?,
     @ColumnInfo(name = "actual_quantity") val actualQuantity: Int?
