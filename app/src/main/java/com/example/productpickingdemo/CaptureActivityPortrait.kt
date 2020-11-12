20f0c965020eb38bcebcebbc7652edcc1ae5485c
Vitaliy <dovbnya1312@gmail.com>
2020-11-12 22:02:58 +0200

    test scanner

---
+++ app/src/main/java/com/example/productpickingdemo/CaptureActivityPortrait.kt
@@ -0,0 +1,6 @@
+package com.example.productpickingdemo
+
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class CaptureActivityPortrait : CaptureActivity() {
+}
--- app/src/main/java/com/example/productpickingdemo/screens/login/LoginFragment.kt
+++ app/src/main/java/com/example/productpickingdemo/screens/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.productpickingdemo.R
 import com.example.productpickingdemo.base.BaseFragment
 import com.example.productpickingdemo.utils.injectViewModel
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : BaseFragment<LoginViewModel>() {
@@ -19,7 +20,14 @@ class LoginFragment : BaseFragment<LoginViewModel>() {
     override fun initialization(view: View, isFirstInit: Boolean) {
 
         btnSubmit.setOnClickListener {
-            navController.navigate(LoginFragmentDirections.actionLoginFragmentToOrdersFragment())
+            val integrator = IntentIntegrator(activity)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES)
+            integrator.setPrompt("Scan a barcode")
+            integrator.setCameraId(0) // Use a specific camera of the device
+            integrator.setOrientationLocked(true)
+            integrator.setBeepEnabled(false)
+            integrator.setBarcodeImageEnabled(true)
+            integrator.initiateScan()
         }
     }
 }
