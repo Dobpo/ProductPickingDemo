e809a7843c42fb43a702f810b21b021d2eec1be3
Alexandr Kravchenko <alex.kravchenko@apptuns.com>
2020-11-13 18:18:37 +0200

    in progress

--- app/src/main/java/com/example/productpickingdemo/screens/shopping_area/ShoppingAreaViewModel.kt
+++ app/src/main/java/com/example/productpickingdemo/screens/shopping_area/ShoppingAreaViewModel.kt
@@ -2,6 +2,16 @@ package com.example.productpickingdemo.screens.shopping_area
 
 import android.app.Application
 import com.example.productpickingdemo.base.view_model.BaseViewModel
+import com.example.productpickingdemo.database.entities.Order
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class ShoppingAreaViewModel @Inject constructor(app: Application) : BaseViewModel(app)
+class ShoppingAreaViewModel @Inject constructor(app: Application) : BaseViewModel(app) {
+    fun removeOrder(order: Order) {
+        CoroutineScope(IO).launch {
+            dbManager.deleteOrder(order)
+        }
+    }
+}
